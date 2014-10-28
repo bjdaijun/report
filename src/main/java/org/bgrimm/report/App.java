ad824d64fe680e9bdcad52a9a519d2d6ede5f3e5
daijun <junsmooth@163.com>
2014-10-29 07:32:07 +0800

    first commit

---
+++ src/main/java/org/bgrimm/report/App.java
@@ -0,0 +1,19 @@
+package org.bgrimm.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+
+@Configuration
+@EnableScheduling
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
